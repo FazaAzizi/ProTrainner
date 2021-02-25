dbe18e81883d0e9f7f473ab2508e6e81f57a7fff
fazaa <faza.azizi.faz@gmail.com>
2021-02-25 13:43:21 +0700

    regisoke

--- app/src/main/java/com/example/protrainner/activity/LoginActivity.java
+++ app/src/main/java/com/example/protrainner/activity/LoginActivity.java
@@ -1,25 +1,43 @@
 package com.example.protrainner.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.protrainner.adapter.LoginAdapter;
 import com.example.protrainner.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
-    ViewPager viewpager;
+
     FloatingActionButton google;
     float v=0;
+
+    EditText inp_email;
+    TextInputLayout lyttext;
+    EditText inp_password;
+    TextView forgetpass, textView;
+    Button button;
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     @Override
@@ -27,33 +45,154 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        tabLayout = findViewById(R.id.tab_layout);
-        viewpager = findViewById(R.id.view_pager);
+        mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        inp_email =findViewById(R.id.email);
+        inp_password =findViewById(R.id.pass);
+        lyttext =findViewById(R.id.passlyt);
+        forgetpass = findViewById(R.id.forgetpass);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.create_acc_member);
         google = findViewById(R.id.fab_google);
 
-        tabLayout.addTab(tabLayout.newTab().setText("Member"));
-        tabLayout.addTab(tabLayout.newTab().setText("Trainer"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        textView.setTranslationX(800);
+        textView.setAlpha(v);
+        textView.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(800).start();
 
 
-        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
-        viewpager.setAdapter(adapter);
+        inp_email.setTranslationX(800);
+        lyttext.setTranslationX(800);
+        forgetpass.setTranslationX(800);
+        button.setTranslationX(800);
+        google.setTranslationX(800);
 
-        viewpager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        google.setTranslationY(300);
-        tabLayout.setTranslationY(300);
-
+        inp_email.setAlpha(v);
+        lyttext.setAlpha(v);
+        forgetpass.setAlpha(v);
+        button.setAlpha(v);
         google.setAlpha(v);
-        tabLayout.setAlpha(v);
 
-        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+        google.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+        inp_email.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(300).start();
+        lyttext.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(500).start();
+        forgetpass.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+        button.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(700).start();
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+                if(mFirebaseUser !=null){
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                }
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inp_email.getText().toString();
+                String pass = inp_password.getText().toString();
+
+                if(email.isEmpty()){
+                    inp_email.setError("Please enter your email");
+                }
+                else if(pass.isEmpty()){
+                    inp_password.setError("Please enter your password");
+                }
+
+                else if(!(email.isEmpty() &&  pass.isEmpty() )){
+                    mAuth.signInWithEmailAndPassword(email,pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(LoginActivity.this,"Login Succesfull",Toast.LENGTH_SHORT).show();
+                            checkIfMember(authResult.getUser().getUid());
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    //mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if(!task.isSuccessful()){
+//                                Toast.makeText(getActivity(),"Login error! coba lagi",Toast.LENGTH_SHORT).show();
+//                            }
+//                            else {
+//                                Toast.makeText(getActivity(),"Login Succesfull",Toast.LENGTH_SHORT).show();
+//                                checkIfMember(task.getUser().getUid());
+//                                Intent inthome =new Intent(getActivity(),MainActivity.class);
+//                                startActivity(inthome);
+//                            }
+//                        }
+//                    });
+                }
+            }
+        });
     }
 
     public void onBackPressed() {
         //do nothing
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser()!=null){
+            DocumentReference df = FirebaseFirestore.getInstance().collection("Akun").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot.getString("isMember").equals("0")){
+                        Intent inthome =new Intent(LoginActivity.this, HomeTrainerActivity.class);
+                        startActivity(inthome);
+                    }
+                    if(documentSnapshot.getString("isMember").equals("1")){
+                        Intent inthome =new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(inthome);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent i = new Intent(LoginActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+    }
+
+    private void checkIfMember(String uid) {
+        DocumentReference df = fStore.collection("Akun").document(uid);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG","onSucces : "+ documentSnapshot.getData());
+                if(documentSnapshot.getString("isMember").equals("0")){
+                    Intent inthome =new Intent(LoginActivity.this, HomeTrainerActivity.class);
+                    startActivity(inthome);
+                }
+                if(documentSnapshot.getString("isMember").equals("1")){
+                    Intent inthome =new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(inthome);
+                }
+            }
+        });
+    }
+
 
 
 }
--- app/src/main/java/com/example/protrainner/activity/RegisterActivity.java
+++ app/src/main/java/com/example/protrainner/activity/RegisterActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,19 +26,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterTrainerActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity{
 
     EditText inpt_email, inpt_name, inpt_pass, inpt_conf_pass;
     Button button;
-    TextView textView;
+    TextView textView,textView2;
     TextInputLayout lyttext1,lyttext2;
     FirebaseAuth mAuth;
     FirebaseFirestore fStore;
+    CheckBox cb1,cb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_trainer);
+        setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -49,18 +52,47 @@ public class RegisterTrainerActivity extends AppCompatActivity {
         inpt_conf_pass= findViewById(R.id.input_conf_pass);
         button= findViewById(R.id.button_acc);
         textView= findViewById(R.id.have_acc);
+        textView2= findViewById(R.id.register_trainer);
+        cb1 = findViewById(R.id.checkBox);
+        cb2 = findViewById(R.id.checkBox2);
+
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isChecked()){
+                    cb2.setChecked(false);
+                }
+            }
+        });
+
+        cb2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isChecked()){
+                    cb1.setChecked(false);
+                }
+            }
+        });
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(RegisterTrainerActivity.this, LoginActivity.class);
+                Intent myIntent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(myIntent);
             }
         });
 
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if(!(cb1.isChecked()||cb2.isChecked())){
+                    Toast.makeText(RegisterActivity.this,"Pilih jenis daftar",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
                 String email = inpt_email.getText().toString();
                 String name = inpt_name.getText().toString();
                 String pass = inpt_pass.getText().toString();
@@ -85,27 +117,45 @@ public class RegisterTrainerActivity extends AppCompatActivity {
                     inpt_conf_pass.setError("Password not match!");
                 }
                 else if(!(email.isEmpty() && name.isEmpty() && pass.isEmpty() && conf_pass.isEmpty() )){
-                    mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(RegisterTrainerActivity.this, new OnCompleteListener<AuthResult>() {
+                    mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (!task.isSuccessful()){
-                                Toast.makeText(RegisterTrainerActivity.this,"Daftar member gagal! Coba lagi",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegisterActivity.this,"Daftar member gagal! Coba lagi",Toast.LENGTH_SHORT).show();
                             }
                             else{
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                DocumentReference df = fStore.collection("Member").document(user.getUid());
+                                DocumentReference df = fStore.collection("Akun").document(user.getUid());
                                 Map<String,Object> userinfo = new HashMap<>();
                                 userinfo.put("Full name",name);
                                 userinfo.put("Email",email);
-                                userinfo.put("isUser", "0");
+                                if(cb1.isChecked()){
+                                    userinfo.put("isMember","0");
+                                }
+                                if(cb2.isChecked()){
+                                    userinfo.put("isMember","1");
+                                }
                                 df.set(userinfo);
-                                startActivity(new Intent(RegisterTrainerActivity.this,MainActivity.class));
-                                Toast.makeText(RegisterTrainerActivity.this,"Create Account Succesfull",Toast.LENGTH_SHORT).show();
+                                if(cb1.isChecked()){
+                                    Intent inthome =new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(inthome);
+                                }
+                                if(cb2.isChecked()){
+                                    Intent inthome =new Intent(RegisterActivity.this, HomeTrainerActivity.class);
+                                    startActivity(inthome);
+                                }
+
+                                Toast.makeText(RegisterActivity.this,"Create Account Succesfull",Toast.LENGTH_SHORT).show();
                             }
+
                         }
                     });
                 }
+
             }
         });
     }
+
+
+
 }
