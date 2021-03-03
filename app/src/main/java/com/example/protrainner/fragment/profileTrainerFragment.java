1b4f3b7341392dbd639638f5e1209a9038b16e21
fazaa <faza.azizi.faz@gmail.com>
2021-03-03 17:19:30 +0700

    trainer profil

--- app/src/main/java/com/example/protrainner/fragment/profileTrainerFragment.java
+++ app/src/main/java/com/example/protrainner/fragment/profileTrainerFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.protrainner.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class profileTrainerFragment extends Fragment {
+
+    TextView namal,ttl,mail,gender,addrs;
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
