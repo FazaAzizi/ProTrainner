f754282f6eae8556e987f94b2ef79bfb02c69709
fazaa <faza.azizi.faz@gmail.com>
2021-03-06 12:20:04 +0700

    zzzzzzzzzz

--- app/src/main/java/com/example/protrainner/activity/MemberListActivity.java
+++ app/src/main/java/com/example/protrainner/activity/MemberListActivity.java
@@ -1,16 +1,83 @@
 package com.example.protrainner.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.protrainner.R;
+import com.example.protrainner.adapter.ListTrainerAdapter;
+import com.example.protrainner.model.Akun;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
-public class MemberListActivity extends AppCompatActivity {
+public class MemberListActivity extends AppCompatActivity implements  ListTrainerAdapter.OnItemClickListener {
+
+    private RecyclerView recyclerView;
+    private ListTrainerAdapter adapter;
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    private CollectionReference cF = fStore.collection("Akun");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_list);
+
+        recyclerView = findViewById(R.id.list_member);
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+        Query query = cF.whereEqualTo("isMember","1");
+        FirestoreRecyclerOptions<Akun> options = new FirestoreRecyclerOptions.Builder<Akun>()
+                .setQuery(query,Akun.class)
+                .build();
+
+        adapter = new ListTrainerAdapter(options);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new ListTrainerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot dS, int position) {
+                Akun akun = dS.toObject(Akun.class);
+                String id = dS.getId();
+
+                Bundle b = new Bundle();
+                b.putString("UID",id);
+                Intent intent = new Intent(MemberListActivity.this, DetailPriceMemberActivity.class);
+                intent.putExtras(b);
+                startActivity(intent);
+
+                Toast.makeText(MemberListActivity.this, "id = " + id,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onItemClick(DocumentSnapshot dS, int position) {
+
     }
 }
--- app/src/main/java/com/example/protrainner/adapter/ListMemberAdapter.java
+++ app/src/main/java/com/example/protrainner/adapter/ListMemberAdapter.java
@@ -1,4 +1,65 @@
 package com.example.protrainner.adapter;
 
-public class ListMemberAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.protrainner.R;
+import com.example.protrainner.model.Akun;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class ListMemberAdapter extends FirestoreRecyclerAdapter<Akun, ListMemberAdapter.ListMemberHolder> {
+    private ListMemberAdapter.OnItemClickListener listener;
+
+    public ListMemberAdapter(@NonNull FirestoreRecyclerOptions<Akun> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull ListMemberAdapter.ListMemberHolder listMemberHolder, int i, @NonNull Akun listMember) {
+        listMemberHolder.tvNama.setText(listMember.getFullname());
+    }
+
+    @NonNull
+    @Override
+    public ListMemberAdapter.ListMemberHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_member
+                , parent, false);
+        return new ListMemberAdapter.ListMemberHolder(v);
+    }
+
+    class ListMemberHolder extends RecyclerView.ViewHolder{
+        TextView tvNama;
+
+        public ListMemberHolder( View itemView) {
+            super(itemView);
+            tvNama = itemView.findViewById(R.id.listnamamember);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null){
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
+        }
+
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(DocumentSnapshot dS, int position);
+    }
+    public void setOnItemClickListener(ListMemberAdapter.OnItemClickListener listener){
+        this.listener=listener;
+    }
+
+
 }
